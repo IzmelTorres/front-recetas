b55fa301c9d4781b7b3acabef2f687813a232bce
IzmelTorres <izmel.torres@123sourcing.com>
2025-03-01 20:01:36 -0600

    cambios

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export const metadata = {
+  title: "Principal",
+  description: "Basdasdasddsas",
+};
+
+export default function Page() {
+  return <h1>Hello Izmel, Next.js!</h1>
+}
---
+++ src/app/recetas/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Blog - Mi App",
+    description: "Bienvenido al blog",
+};
+
+export default function BlogPage() {
+    return <h1>Bienvenido al Blog</h1>;
+}
